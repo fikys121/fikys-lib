970416689f2a0c17c8cf7c0c54912c56bb981cb8
fikys <lolik.1300@mail.ru>
2023-03-30 12:43:40 +0300

    карточка

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
-new Swiper('.anime_slider',{
-    loop:true,
-    slidesPerView:3.7,
-    spaceBetween:20,
-    freeMode:true,   
-})
+// new Swiper('.anime_slider',{
+//     loop:true,
+//     slidesPerView:3.7,
+//     spaceBetween:20,
+//     freeMode:true,   
+// })
